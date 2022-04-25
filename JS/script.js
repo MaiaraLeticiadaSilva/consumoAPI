cc1c048c03dfcd3870732753b2a4fd14eff7856a
Maiara Silva <maiaraleticiadasilva@gmail.com>
2022-04-25 08:01:52 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,40 @@
+document.querySelector("#input").onclick=function(){
+    fetch("https://h-apigateway.conectagov.estaleiro.serpro.gov.br/oauth2/jwt-token").then(
+        function(res){
+            console.log(res.json())
+            return res.json();
+        }
+    ).then(function(res){
+        let tbody = document.querySelector("#cep-t")
+            let tr = document.createElement("tr")
+            let tdcep = document.createElement("td")
+            let txcep = document.createTextNode(res.cep)
+            tdcep.append(txcep)
+            tr.append(tdcep)
+            let tduf = document.createElement("td")
+            let txuf = document.createTextNode(res.uf)
+            tduf.append(txuf)
+            tr.append(tduf)
+            let tdCidade = document.createElement("td")
+            let txCidade = document.createTextNode(res.cidade)
+            tdCidade.append(txCidade)
+            tr.append(tdCidade)
+            let tdBairro = document.createElement("td")
+            let txBairro = document.createTextNode(res.bairro)
+            tdBairro.append(txBairro)
+            tr.append(tdBairro)
+            let tdEndereço = document.createElement("td")
+            let txEndereço = document.createTextNode(res.endereco)
+            tdEndereço.append(txEndereço)
+            tr.append(tdEndereço)
+            let tdComp = document.createElement("td")
+            let txComp = document.createTextNode(res.complemento)
+            tdComp.append(txComp)
+            tr.append(tdComp)
+            let tdCod = document.createElement("td")
+            let txCod = document.createTextNode(res.codigoIBGE)
+            tdCod.append(txCod)
+            tr.append(tdCod)
+            tbody.append(tr)
+    })
+}
